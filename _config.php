5339b0a3e7c53de48de17a83ee2ae41c90dfe598
Rainer Spittel <fb3rasp@gmail.com>
2014-04-23 16:08:44 +1200

    MINOR added route to MetadataAdmin from config.php to routes.yml.

--- _config.php
+++ _config.php
@@ -4,17 +4,11 @@
 Requires XSLT-Processor, i.e.
 
 >> sudo apt-get install php5-xsl
-
 */
 
 // Change this field to anything except 'setup' do set the configuration fields 
 // to read-only.
 CataloguePage::set_site_status('setup');
-
-Director::addRules(100, array(
-	'admin/metadata' => "MetadataAdmin",
-));
-
 
 // Add model admin for the metadata entries.
 CMSMenu::add_menu_item("metadata", 'Metadata', 'admin/metadata', "MetadataAdmin" );
